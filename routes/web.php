0c066e91799a7c0c8fcd7ce8dc2765df432f0d8f
DZAKI KHOTHIR <dzaki.khothir123@gmail.com>
2026-05-21 16:07:39 +0700

    remove search bar on profile page

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,7 @@ Route::middleware(['auth'])->group(function () {
         Route::prefix('profile')->name('profile.')->group(function () {
             Route::get('/', [ProfileController::class, 'index'])->name('index');
             Route::patch('/', [ProfileController::class, 'update'])->name('update');
+            Route::put('/password', [ProfileController::class, 'updatePassword'])->name('password'); 
             Route::delete('/', [ProfileController::class, 'destroy'])->name('destroy');
         });
 
